69cadef15acb7d34bcc5f098f23f8b2271ca479a
Mahoyaail <angel_aham@hotmail.com>
2021-01-15 23:02:47 +0300

    initial commit

---
+++ maaa1.py
@@ -0,0 +1,18 @@
+
+
+name = input("دخل اسم : ")
+place = input(" اسم مكان : ")
+stolen = input(" بتحب شنو؟ : ")
+friend = input("من الاصحاب؟ : ")
+adj1 = input(" دخل صفة : ")
+adj2 = input(" دخل صفة تانيه : ")
+adj3 = input(" دخل صفة اخيرة : ")
+
+print (" طلع " + name)
+print (" من " + place)
+print (" سرقو منو " + stolen)
+print (" كلم " + friend)
+print (" قال ليه انت " + adj1+"ان")
+print (" قال ليه انا " + adj2+"ت")
+print (" ورجع البيت " + adj3+"ان")
+
